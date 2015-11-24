9706319c661a02686c30ca2db5dfa579daf22b23
Philip Newborough <corenominal@corenominal.org>
2015-11-24 20:05:35 +0000

    Too much?

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php
 /**
+ *                                             _             _
+ *     ___ ___  _ __ ___ _ __   ___  _ __ ___ (_)_ __   __ _| |
+ *    / __/ _ \| '__/ _ \ '_ \ / _ \| '_ ` _ \| | '_ \ / _` | |
+ *   | (_| (_) | | |  __/ | | | (_) | | | | | | | | | | (_| | |
+ *    \___\___/|_|  \___|_| |_|\___/|_| |_| |_|_|_| |_|\__,_|_|
+ *
+ *   Webmaster: Philip Newborough
+ *   Contact: corenominal [at] corenominal.org
+ *   Twitter: @corenominal
+ *   From: Lincoln, United Kingdom
+ *
  * The main template file
  *
  * This is the most generic template file in a WordPress theme
