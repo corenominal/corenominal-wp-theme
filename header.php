b87db48f80e9c551c8b146dc2d1a930e373dad52
Philip Newborough <corenominal@corenominal.org>
2015-11-24 20:40:43 +0000

    Adjust header to be more granular

--- header.php
+++ header.php
@@ -1,19 +1,12 @@
 <?php
 /**
- * The default header template for displaying the header.
- *
- * Displays all of the head element and opening body element.
+ * The default header template for displaying the, erm, header.
  */
-?><!DOCTYPE html>
-<html <?php language_attributes(); ?> class="no-js">
-<head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width">
-	<title><?php wp_title( '|', true, 'right' ); ?><?php bloginfo('name'); ?></title>
-	<link rel="profile" href="http://gmpg.org/xfn/11">
-	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-	<link rel="alternate" type="application/rss+xml" title="RSS Feed | <?php bloginfo('name'); ?>" href="<?php bloginfo( 'rss2_url' ); ?>">
-	<link rel="stylesheet" type="text/css" href="<?php echo esc_url( get_template_directory_uri() ); ?>/css/root.css">
-	<?php wp_head(); ?>
-</head>
-<body>
+
+/**
+ * Include the meta.
+ */
+require get_template_directory() . '/inc/meta.php';
+?>
+
+<!-- HTML header code to go here -->
