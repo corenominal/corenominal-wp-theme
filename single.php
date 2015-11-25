6ea12b64f3a73c51aca445f6761db05542b252af
Philip Newborough <corenominal@corenominal.org>
2015-11-25 19:59:50 +0000

    Bare minimum to enable comments

---
+++ single.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ *                                             _             _
+ *     ___ ___  _ __ ___ _ __   ___  _ __ ___ (_)_ __   __ _| |
+ *    / __/ _ \| '__/ _ \ '_ \ / _ \| '_ ` _ \| | '_ \ / _` | |
+ *   | (_| (_) | | |  __/ | | | (_) | | | | | | | | | | (_| | |
+ *    \___\___/|_|  \___|_| |_|\___/|_| |_| |_|_|_| |_|\__,_|_|
+ *
+ *   Webmaster: Philip Newborough
+ *   Contact: corenominal [at] corenominal.org
+ *   Twitter: @corenominal
+ *   From: Lincoln, United Kingdom
+ *
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * e.g., it puts together the home page when no home.php file exists.
+ *
+ * Learn more: {@link https://codex.wordpress.org/Template_Hierarchy}
+ */
+
+/**
+ * Pull in the header
+ */
+get_header();
+
+/**
+ * Sanity check
+ */
+if ( have_posts() ) :
+
+	/**
+	 * Start the loop
+	 */
+	while ( have_posts() ) :
+		the_post();
+?>
+
+		<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+		<small><?php the_date(); ?> - <?php the_time(); ?></small>
+		
+		<?php
+
+		the_content();
+		
+		comments_template();
+
+		?>
+	
+<?php
+	/**
+	 * End the loop
+	 */
+	endwhile;
+
+/**
+ * We may not have any posts. Doh!
+ */
+else :
+
+	echo 'Oops, it looks like something went horribly wrong.';
+
+endif;
+
+/**
+ * Pull in the footer
+ */
+get_footer();
