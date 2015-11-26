0c932575c2fa6310901d8692bdf60bac2d0a5471
Philip Newborough <corenominal@corenominal.org>
2015-11-26 18:25:34 +0000

    Dump and die debugging function

--- functions.php
+++ functions.php
@@ -8,3 +8,13 @@
  * Include random stuff that does not fit elsewhere.
  */
 require get_template_directory() . '/functions/misc.php';
+
+/**
+ * Dump and die, a handy function for debugging. Idea pilfered from Laravel.
+ */
+function dd( $data )
+{
+	echo '<pre><code>';
+	var_dump( $data );
+	echo '</code></pre>';
+}
