497a6990fe498ab25b48b453512e4726be094ff1
Philip Newborough <corenominal@corenominal.org>
2015-11-26 19:39:16 +0000

    Credit where credit is due.

--- footer.php
+++ footer.php
@@ -10,7 +10,7 @@ wp_footer();
 ?>
 	<footer>
 		&mdash;&mdash;
-		<p><small>&copy; <?php echo date('Y'); ?> Philip Newborough.</small></p>
+		<p><small>&copy; <?php echo date('Y'); ?> Philip Newborough. Proudly powered by <a href="http://wordpress.org/">WordPress</a>.</small></p>
 	</footer>
 	<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/root.js"></script>
 	</body>
