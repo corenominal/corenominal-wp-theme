c07e6a1e1c4539d2907381fb37db6f7bee2e3661
Philip Newborough <corenominal@corenominal.org>
2015-12-03 14:44:10 +0000

    Add default category template

---
+++ category.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * The default category template.
+ */
+
+/**
+ * Pull in the header
+ */
+get_header();
+
+/**
+ * Show which category we are dealing with
+ */
+echo '<h1 class="taxonomy-title">Category: ' . single_cat_title( '', false ) . '</h1>';
+
+/**
+ * Sanity check
+ */
+if ( have_posts() ) :
+
+	/**
+	 * Start the loop
+	 */
+	while ( have_posts() ) :
+		the_post();
+		$date = date( "F j, Y" , strtotime( $post->post_date ) );
+?>
+
+		<article class="h-entry post-summary">
+
+			<h2>
+				<a class="p-name u-url" href="<?php the_permalink(); ?>"><?php the_title() ?></a>
+			</h2>
+		
+			<div class="p-description">
+				<?php the_excerpt() ?>
+			</div>
+
+			<footer>
+				
+				<p class="meta"><?php echo $date . ' '; the_time() ?></p>
+				
+				<?php if( get_option( 'corenominal_show_cats', 'false' ) == 'true' ): ?>
+				<p class="meta">Filed under: <?php the_category( ' ' ) ?></p>
+				<?php endif; ?>
+				
+				<?php if( get_option( 'corenominal_show_tags', 'true' ) == 'true' ): ?>
+				<p class="meta"><?php the_tags() ?></p>
+				<?php endif; ?>
+
+			</footer>
+
+		</article>
+		
+		<?php
+		
+	/**
+	 * End the loop
+	 */
+	endwhile;
+?>
+
+<?php next_posts_link( '< Older posts' ) ?><br><?php previous_posts_link( 'Newer posts >' ) ?>
+
+<?php
+
+/**
+ * We may not have any posts. Doh!
+ */
+else :
+
+	echo 'Oops, it looks like something went horribly wrong.';
+
+endif;
+
+/**
+ * Pull in the footer
+ */
+get_footer();
