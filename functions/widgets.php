51eeb31207e2d0e2de0920acaf15e48cf4242c75
Philip Newborough <corenominal@corenominal.org>
2015-12-10 19:43:22 +0000

    Widget for RSS feeds

--- functions/widgets.php
+++ functions/widgets.php
@@ -118,7 +118,7 @@ class corenominal_recent_links_widget extends WP_Widget{
 		$links->query( 'post_type=link&posts_per_page=' . $numberOfLinks );
 		if( $links->found_posts > 0)
 		{
-			echo '<ul class="recent_links_widget">';
+			echo '<ul class="recent_links_widget recent">';
 				while ($links->have_posts())
 				{
 					$links->the_post();
@@ -212,7 +212,7 @@ class corenominal_recent_posts_widget extends WP_Widget{
 		$links->query( 'post_type=post&posts_per_page=' . $numberOfPosts );
 		if( $links->found_posts > 0)
 		{
-			echo '<ul class="recent_posts_widget">';
+			echo '<ul class="recent_posts_widget recent">';
 				while ($links->have_posts())
 				{
 					$links->the_post();
@@ -234,3 +234,82 @@ class corenominal_recent_posts_widget extends WP_Widget{
 
 }
 register_widget( 'corenominal_recent_posts_widget' );
+
+/**
+ * Create "Recent" widget for custom post type - 'link'
+ */
+class corenominal_rss_widget extends WP_Widget{
+	
+	function __construct()
+	{
+		parent::__construct(
+			'corenominal_rss_widget', // Base ID
+			'corenominal - RSS Links', // Name
+			array('description' => __( 'Displays links to your RSS feeds.' ))
+		   );
+	}
+
+	function update( $new_instance, $old_instance )
+	{
+			$instance = $old_instance;
+			$instance['title'] = strip_tags($new_instance['title']);
+			return $instance;
+	}
+
+	function form( $instance )
+	{
+		if( $instance )
+		{
+			$title = esc_attr( $instance['title'] );
+		}
+		else
+		{
+			$title = '';
+		}
+		?>
+			<p>
+			<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title', 'corenominal_rss_widget'); ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
+			</p>
+		<?php
+	}
+
+	function widget( $args, $instance )
+	{
+		extract( $args );
+		$title = apply_filters('widget_title', $instance['title']);
+		
+		if ( $title )
+		{
+			$title =  $before_title . $title . $after_title;
+		}
+		
+		$count_posts = wp_count_posts( 'post' );
+		$published_posts = $count_posts->publish;
+		$count_posts = wp_count_posts( 'link' );
+		$published_links = $count_posts->publish;
+		$feeds = '';
+		
+		if( $published_posts )
+		{
+			$feeds .= '<li><i class="fa fa-rss"></i> <a href="' . site_url() . '/feed/?post_type=post">Subscribe to Posts</a></li>';
+		}
+		
+		if( $published_links )
+		{
+			$feeds .= '<li><i class="fa fa-rss"></i> <a href="' . site_url() . '/feed/?post_type=link">Subscribe to Links</a></li>';
+		}
+		
+		if($feeds != '')
+		{
+			echo $before_widget;
+			echo $title;
+			echo '<ul class="subscriptions">';
+			echo $feeds;
+			echo '</ul>';
+			echo $after_widget;
+		}
+	}
+
+}
+register_widget( 'corenominal_rss_widget' );
