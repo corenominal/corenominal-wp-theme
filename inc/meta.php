9f7fd10918f5965d7e557340915467e88534ae45
Philip Newborough <corenominal@corenominal.org>
2015-12-11 09:54:53 +0000

    Apple touch icon ref

--- inc/meta.php
+++ inc/meta.php
@@ -8,7 +8,9 @@
 <html <?php language_attributes(); ?> class="no-js">
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width">
+<meta http-equiv="x-ua-compatible" content="ie=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="apple-touch-icon" href="<?php echo site_url(); ?>/apple-touch-icon.png">
 <?php if( is_home() ): ?>
 <title><?php bloginfo('name')?> | <?php bloginfo('description'); ?></title>
 <?php else: ?>
