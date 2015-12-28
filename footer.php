3fedf72e82c2181131f114e647389a91f957652b
Philip Newborough <corenominal@corenominal.org>
2015-12-28 11:59:38 +0000

    Add jquery waypoints

--- footer.php
+++ footer.php
@@ -26,6 +26,7 @@ wp_footer();
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script>window.jQuery || document.write('<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/vendor/jquery-1.11.3.min.js"><\/script>')</script>
 <script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/vendor/jquery.fitvids.js"></script>
+<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/vendor/jquery.waypoints.js"></script>
 <script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/root.js"></script>
 <?php if ( is_post_type_archive( 'doodle' ) || is_singular( 'doodle' ) ): ?>
 <script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/vendor/jquery.magnific-popup.js"></script>
