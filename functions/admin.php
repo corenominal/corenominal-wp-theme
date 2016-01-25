b37c3d4b4801e9b6434feba76bd7f2dea743cdea
Philip Newborough <corenominal@corenominal.org>
2016-01-25 20:25:53 +0000

    Remove metadata functions, moved to plugin

--- functions/admin.php
+++ functions/admin.php
@@ -99,23 +99,6 @@ function corenominal_admin_menu()
 	add_action( 'admin_init', 'corenominal_social_settings' );
 
 	/**
-	 * Metadata settings submenu item
-	 */
-	add_submenu_page( 
-		'corenominal', // parent slug
-		'Metadata', // page title
-		'Metadata', // menu title
-		'manage_options', // capability
-		'corenominal_metadata', // slug
-		'corenominal_metadata_callback' // callback function
-	 	);
-
-	/**
-	 * Activate metadata options 
-	 */
-	add_action( 'admin_init', 'corenominal_metadata_settings' );
-
-	/**
 	 * API key submenu item
 	 */
 	add_submenu_page( 
@@ -154,11 +137,6 @@ require get_template_directory() . '/functions/views/admin_corenominal_design.ph
  * corenominal_social_callback()
  */
 require get_template_directory() . '/functions/views/admin_corenominal_social.php';
-
-/**
- * corenominal_metadata_callback()
- */
-require get_template_directory() . '/functions/views/admin_corenominal_metadata.php';
 
 /**
  * corenominal_apikey_callback()
