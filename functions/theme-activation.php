b4545e6700164c0c6e6d51a9ed2c10cb17cc9742
Philip Newborough <corenominal@corenominal.org>
2016-01-26 13:40:37 +0000

    Remove smiley/emoji disabling, moved to plugin

--- functions/theme-activation.php
+++ functions/theme-activation.php
@@ -6,12 +6,6 @@
  *  - switch_theme is triggered when the blog's theme is changed. Specifically, it fires after the theme has been switched but before the next request. Theme developers should use this hook to do things when their theme is deactivated. 
  */
 
-function corenominal_set_theme_options()
-{
-	update_option( 'use_smilies', false );
-}
-add_action( 'after_switch_theme', 'corenominal_set_theme_options' );
-
 /**
  * Set-up war demo database tables
  */
